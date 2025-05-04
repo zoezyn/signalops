556b18b22af42f29059bb8dc01f3526a85faae84
digit-ink <zan.mcnaught@gmail.com>
2025-05-03 19:12:46 -0700

    add gptWebSearch() fn

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,25 @@
+"use client"
+
+import OpenAI from "openai";
 import Image from "next/image";
+import { useEffect } from "react";
+
+const gptWebSearch = async () => {
+  const client = new OpenAI();
+  const response = await client.responses.create({
+    model: "gpt-4.1",
+    tools: [ { type: "web_search_preview" } ],
+    input: "What was a positive news story from today?",
+  });
+
+  console.log(response.output_text);
+};
 
 export default function Home() {
+  useEffect(() => {
+    gptWebSearch();
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
